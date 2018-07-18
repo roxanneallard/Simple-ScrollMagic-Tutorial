96191e6d20aeef97d84093bc1144b6c5ecb80c16
Roxanne Allard <info@roxanneallard.nl>
2018-07-18 13:39:02 +0200

    update
    update

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 (function($) {
-    var wh = window.innerHeight,
-        $iphone = $('.iphone'),
-        $innerS1 = $('.innerS1'),
-        $innerS2 = $('.innerS2'),
-        $innerS3 = $('.innerS3'),
-        $innerS4 = $('.innerS4'),
-        $screenHat = $('.screenHat'),
-        $screenA = $('.screenA'),
-        $screenB = $('.screenB'),
-        $screenC = $('.screenC');
+    var wh = window.innerHeight;
+    var $iphone = $('.iphone');
+    var $innerS1 = $('.innerS1');
+    var $innerS2 = $('.innerS2');
+    var $innerS3 = $('.innerS3');
+    var $innerS4 = $('.innerS4');
+    var $screenHat = $('.screenHat');
+    var $screenA = $('.screenA');
+    var $screenB = $('.screenB');
+    var $screenC = $('.screenC');
 
         // init
 var ctrl = new ScrollMagic.Controller({
@@ -29,18 +29,26 @@ $("section").each(function() {
 });
 
 // iPhone intro animation Tween
-var iphoneIntro = TweenMax.from($iphone, 1, {
-    yPercent: 50,
-    xPercent: 100,
-    ease: Cubic.easeOut
-});
+// var iphoneIntro = TweenMax.from($iphone, 1, {
+//     yPercent: 50,
+//     xPercent: 100,
+//     ease: Cubic.easeOut
+// });
+
+// iPhone intro animation Timeline
+var iphoneIntroTl = new TimelineMax();
+iphoneIntroTl
+    .from($iphone, 1, {yPercent: 50,xPercent: 100, ease: Power4.easeInOut})
+    .to($innerS1, 0.5, {opacity: 0, yPercent: -5, scale: 0.98}, '0');
 
 // iPhone back to stylesheet position
 new ScrollMagic.Scene({
     duration: '70%'
 })
-.setTween(iphoneIntro)
+.setTween(iphoneIntroTl)
 .triggerElement($('body')[0])
 .addTo(ctrl);
+
+
 
 })(jQuery);
